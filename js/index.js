33b54b7ba4b9ace7662a0927a38066dd4ada696d
Peabody1920 <89600922+Peabody1920@users.noreply.github.com>
2022-02-27 20:39:31 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,13 +4,13 @@ search.addEventListener("input", show)
 
 function show(element){
 
-    let inputVal = search.value
+    let inputVal = search.value.toUpperCase()
     let cardBody = document.getElementsByClassName("card")
     
 
     Array.from(cardBody).forEach(function(element){
        
-        let cardTxt = element.getElementsByTagName("h6")[0].innerText;
+        let cardTxt = element.getElementsByTagName("h6")[0].innerText.toUpperCase();
         
         
         
